46df10a5b315f43583ebea535e19bba1a5d58d5c
dellis2cs <aprules68@yahoo.com>
2024-08-15 15:53:00 -0700

    complete js exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const getAge = function (birth, death) {
+  if (!death) {
+    death = new Date().getFullYear();
+  }
+  return death - birth;
+};
 
+const findTheOldest = function (arr) {
+  let ages = arr.map((person) =>
+    getAge(person.yearOfBirth, person.yearOfDeath)
+  );
+  for (let i = 0; i < ages.length; i++) {
+    arr[i]["age"] = ages[i];
+  }
+  ages = ages.sort((a, b) => a - b).reverse();
+
+  return arr.find((elem) => elem.age === ages[0]);
 };
 
 // Do not edit below this line
